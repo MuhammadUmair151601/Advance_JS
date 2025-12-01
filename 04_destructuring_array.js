d98488c950cc6de5ccc69b7e1d2171ad162d0b87
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-01 10:22:06 +0500

    destructuring of an array done

---
+++ 04_destructuring_array.js
@@ -0,0 +1,24 @@
+// if we want to assign the values of an array to the variable we use:
+// let arr = ["Ali","ahmad","raza","shahid"];
+// let name1 = arr[0];
+// let name2 = arr[1];
+// let name3 = arr [2];
+// let name4 = arr[3];
+
+// console.log(name2)
+// console.log(name3)
+// console.log(name3)
+
+// Now in ES6 there is a new method called arrays destructuring:
+// let user = ["ali",23, "peshawar","BSSE"];
+// let [name,age,city ] = user;
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+// we can also do this in functions:
+// function user (){
+//   return["ali",45,"peshawar"]
+// }
+// let [name ,age ,city] = user();
+// console.log(name,age,city);
