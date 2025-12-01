898d648041bffff68ff158c14862b1c327533796
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-01 09:23:00 +0500

    object literals done

---
+++ 03_objLiterals.js
@@ -0,0 +1,22 @@
+// object literals are used when we want to put a variable inside an object:
+// Example:
+// let name = "m.umair"
+// let age = 21
+// let course = "BSIT"
+// let details = {
+//   name,
+//   age,
+//   course
+// }
+// console.log(details);
+
+// similarly we can also use the method like:
+// let n = name;
+// let a = age;
+// let c = course;
+// let obj = {
+//   [n] : "Ali",
+//   [a] : 23,
+//   [c] : "BSIT"
+// }
+// console.log(obj)
