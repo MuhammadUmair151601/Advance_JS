51636a79740d64fce63e720ecca726b98377db82
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-01 10:52:43 +0500

    objects destructuring done

---
+++ 05_destructuring0f_obj.js
@@ -0,0 +1,12 @@
+// just like arrays objects can also be destructured means we can assign the values of object to a variable
+
+// Example:
+let obj = {
+  name : "ahmed",
+  age : 23,
+  city : "peshawar",
+  code : 1234
+}
+
+let {name,age,city,code} = obj
+console.log(name,age,city,code)
