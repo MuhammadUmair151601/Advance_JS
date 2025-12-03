a99ece587378b0aa9f0278bdec4fe8641d6e5bc0
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-03 09:26:33 +0500

    Promises concept cleared

---
+++ 08_Promises.js
@@ -0,0 +1,2 @@
+// promises in javascript keeps the flow of the asynchronous js as js is single threaded and it avoids the callback hell keeps the code fast easy to read and debug easily.it helps you avoid blocking the main thread. promises are like containers for the storing of the future value:
+
