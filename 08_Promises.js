1f3f7364d7133ea67981f0b0360133da6f6572a8
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-03 10:47:25 +0500

    result and error applied in promise

--- 08_Promises.js
+++ 08_Promises.js
@@ -1,2 +1,38 @@
 // promises in javascript keeps the flow of the asynchronous js as js is single threaded and it avoids the callback hell keeps the code fast easy to read and debug easily.it helps you avoid blocking the main thread. promises are like containers for the storing of the future value:
 
+// There are three conditions for promises:
+// pending
+// fulfilled if fulfilled then()
+// reject if rejected catch()
+
+
+// First example problem:
+// let success = false;
+// let prom = new Promise(function(resolve,reject){
+//   if(success){
+//     resolve("i am passed")
+//   }
+//   else{
+//     reject("i am failed")
+//   }
+// });
+// console.log(prom)
+
+function prom(complete){
+  return new Promise(function(resolve,reject){
+  if(complete){
+    resolve("i am passed")
+  }
+  else{
+    reject("i am failed")
+  }
+});
+}
+let onfulfilment = (result) =>{
+  console.log(result)
+}
+let onRejection=(error)=>{
+  console.log(error)
+}
+prom(false).then(onfulfilment);
+prom(false).then(onRejection)
