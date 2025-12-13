03bc2b3c6ef430e4746a41d959f554909e5f3528
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-13 14:09:00 +0500

    Strict Mode done

---
+++ 16_strictMode.js
@@ -0,0 +1,11 @@
+// sometime the compiler and console ignores the syntax error which causes the huge bugs generation in the future:
+// Example
+
+// a = 10;
+// console.log(a);
+
+// // this will provide the correct answer:
+// "use strict"
+// a = 10;
+// console.log(a);
+// // this will give syntax error 
