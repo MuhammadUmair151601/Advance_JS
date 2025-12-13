cef7acc323d0ed50d2ab849beb13b9e0758d25da
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-13 14:00:46 +0500

    iterators done

---
+++ 14_Iterators.js
@@ -0,0 +1,11 @@
+// Iterators are used in place of normal loops as we know the normla loops are not in our handle it prints all the values which we want while in iterators we can manually get the value by our own choice:
+// Example:
+// var x = ["a","b","c","d"];
+// let y = x[Symbol.iterator]();
+// console.log(y.next());
+// let a = 12;
+// let b = 12;
+// let sum = a + b;
+// console.log(sum);
+// console.log(y.next())
+// Now i printed the value of 1st index after doing an operation in the code:
