6c21d6887b6580367f0a11a0df14db7a0ea3642e
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-13 14:05:53 +0500

    generators done

---
+++ 15_generators.js
@@ -0,0 +1,15 @@
+// // special functions* used to pause when we want to continue when we want:
+// // Example:
+// function* counter(){
+//   let i = 1;
+//   while(true){
+//     yield i++;
+//   }
+// }
+// const count = counter();
+// console.log(count.next().value)
+// console.log(count.next().value)
+// console.log(count.next().value)
+// console.log(count.next().value)
+// console.log(count.next().value)
+// upto so on..............
